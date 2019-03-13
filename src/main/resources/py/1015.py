cfda76fca804ca8883276e0aa7bdae87c666e816
chenfeihao <chenfeihao@corp.netease.com>
2019-03-13 13:44:49 +0800

    @dev 导入py算法模型

---
+++ src/main/resources/py/1015.py
@@ -0,0 +1,74 @@
+import keras
+from tensorflow.contrib.keras.api.keras.preprocessing.image import ImageDataGenerator,img_to_array
+from keras.models import Sequential
+from keras.layers.core import Dense, Dropout, Activation, Flatten
+from keras.layers.advanced_activations import PReLU
+from keras.layers.convolutional import Conv2D, MaxPooling2D,ZeroPadding2D
+from keras.preprocessing.image import load_img, img_to_array
+from keras.optimizers import  SGD, Adam
+import cv2,os
+import numpy as np
+
+
+def __CNN__():
+    model = Sequential()#218*178*3
+    # model.add(Conv2D(32, (3, 3), input_shape=(254, 309, 3)))
+    model.add(Conv2D(32, (3, 3), input_shape=(600, 800, 3)))
+    model.add(Activation('relu'))
+    model.add(MaxPooling2D(pool_size=(2, 2)))
+
+    model.add(Conv2D(32, (3, 3)))
+    model.add(Activation('relu'))
+    model.add(MaxPooling2D(pool_size=(2, 2)))
+
+    model.add(Conv2D(64, (3, 3)))
+    model.add(Activation('relu'))
+    model.add(MaxPooling2D(pool_size=(2, 2)))
+
+    model.add(Conv2D(64, (3, 3)))
+    model.add(Activation('relu'))
+    model.add(MaxPooling2D(pool_size=(2, 2)))
+
+    model.add(Conv2D(64, (3, 3)))
+    model.add(Activation('relu'))
+    model.add(MaxPooling2D(pool_size=(2, 2)))
+
+    model.add(Flatten())
+    model.add(Dense(128))
+    model.add(Activation('relu'))
+
+    model.add(Dense(128))
+    model.add(Activation('relu'))
+    model.add(Dropout(0.5))
+
+    model.add(Dense(64))
+    model.add(Activation('relu'))
+    model.add(Dropout(0.5))
+
+    # model.add(Dense(32))
+    # model.add(Activation('relu'))
+    # model.add(Dropout(0.5))
+
+    model.add(Dense(1))
+    model.summary()
+    return model
+model1 = __CNN__()
+model1.load_weights('CNN_model_final.h5')
+imageData = cv2.imread('2018Jun16-17.03.14.jpg')
+#imageData = cv2.resize(imageData,254,309,3)
+imageData = np.asarray(imageData)
+#print('img.shape', imageData.shape)
+height = len(imageData)
+width = len(imageData[0])
+#imageData = imageData[15:  127, 30 : width-30]
+#print(imageData.shape)
+#imageData = imageData.reshape(1,112, 249, 3)
+#print(imageData.shape)
+#imageData = preprocess_input(imageData)
+a = model1.predict(imageData,batch_size=1,verbose=0)
+#print(a)
+print(a[0][0])
+
+f = open("out.txt",w)
+f.write(str(a[0][0]))
+f.close()
---
+++ src/main/resources/py/1016.py
@@ -0,0 +1,72 @@
+import keras
+from tensorflow.contrib.keras.api.keras.preprocessing.image import ImageDataGenerator,img_to_array
+from keras.models import Sequential
+from keras.layers.core import Dense, Dropout, Activation, Flatten
+from keras.layers.advanced_activations import PReLU
+from keras.layers.convolutional import Conv2D, MaxPooling2D,ZeroPadding2D
+from keras.preprocessing.image import load_img, img_to_array
+from keras.optimizers import  SGD, Adam
+import cv2
+import tensorflow as tf
+import numpy as np
+import os
+
+def __CNN__():
+    model = Sequential()#218*178*3
+    # model.add(Conv2D(32, (3, 3), input_shape=(254, 309, 3)))
+    model.add(Conv2D(32, (3, 3), input_shape=(112, 249, 3)))
+    model.add(Activation('relu'))
+    model.add(MaxPooling2D(pool_size=(2, 2)))
+
+    model.add(Conv2D(32, (3, 3)))
+    model.add(Activation('relu'))
+    model.add(MaxPooling2D(pool_size=(2, 2)))
+
+    model.add(Conv2D(64, (3, 3)))
+    model.add(Activation('relu'))
+    model.add(MaxPooling2D(pool_size=(2, 2)))
+
+    model.add(Conv2D(64, (3, 3)))
+    model.add(Activation('relu'))
+    model.add(MaxPooling2D(pool_size=(2, 2)))
+
+    model.add(Conv2D(64, (3, 3)))
+    model.add(Activation('relu'))
+    model.add(MaxPooling2D(pool_size=(2, 2)))
+
+    model.add(Flatten())
+    model.add(Dense(128))
+    model.add(Activation('relu'))
+
+    model.add(Dense(128))
+    model.add(Activation('relu'))
+    model.add(Dropout(0.5))
+
+    model.add(Dense(64))
+    model.add(Activation('relu'))
+    model.add(Dropout(0.5))
+
+    # model.add(Dense(32))
+    # model.add(Activation('relu'))
+    # model.add(Dropout(0.5))
+
+    model.add(Dense(1))
+    model.summary()
+    return model
+model1 = __CNN__()
+model1.load_weights('CNN_model_final3.h5')
+imageData = cv2.imread('ACB88.BMP')
+imageData = np.asarray(imageData)
+#print('img.shape', imageData.shape)
+height = len(imageData)
+width = len(imageData[0])
+imageData = imageData[15:  127, 30 : width-30]
+#print(imageData.shape)
+imageData = imageData.reshape(1,112, 249, 3)
+#print(imageData.shape)
+a = model1.predict(imageData,batch_size=1,verbose=0)
+print(a[0][0])
+
+f = open("out_houdu.txt","w")
+f.write(str(a[0][0]))
+f.close()
