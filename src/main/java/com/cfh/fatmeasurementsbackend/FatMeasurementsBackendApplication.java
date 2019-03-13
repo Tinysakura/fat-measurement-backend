fbb084cfdde84cd9e377d6c46828d48ddb4f3335
chenfeihao <chenfeihao@corp.netease.com>
2019-03-13 12:53:15 +0800

    @dev 初始化工程

---
+++ src/main/java/com/cfh/fatmeasurementsbackend/FatMeasurementsBackendApplication.java
@@ -0,0 +1,13 @@
+package com.cfh.fatmeasurementsbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FatMeasurementsBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FatMeasurementsBackendApplication.class, args);
+    }
+
+}
